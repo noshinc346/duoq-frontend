273295d5d1a29b1156f42ea6a4fc2b25cd276dbd
Jenn Bourke <jennfbourke@gmail.com>
2024-04-11 09:54:18 -0400

    Game images and css added

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import './Nav.css'
+import { NavLink } from 'react-router-dom'
 
-function Nav() {
+const authenticatedOptions = (
+  <>
+  <NavLink className="authlink" to ="/profile">Profile</NavLink> 
+  <NavLink className="authlink" to ="/sign-out">Sign Out</NavLink>
+  </>
+)
+  
+
+const unauthenticatedOptions = (
+  <>
+  <button className="login-button"><NavLink className="link" to="/login">Login</NavLink></button>
+  </> 
+)
+
+const alwaysOptions = (
+  <>
+  <NavLink className="logo-name" to="/">DuoQ</NavLink>
+  </>
+)
+
+function Nav({ user }) {
   return (
-    <div>Nav</div>
+    <div className="nav">
+      <nav className="navbar">
+        {alwaysOptions}
+        {user ? authenticatedOptions : unauthenticatedOptions}
+
+        {/* <p className="logo">DuoQ</p>
+        <button><NavLink to="/" className="login-button">Login</NavLink></button> */}
+      </nav>
+
+    </div>
   )
 }
 
