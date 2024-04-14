84dcbb784234afc67fbab3c0228b285ada0430d0
Jenn Bourke <jennfbourke@gmail.com>
2024-04-14 16:09:21 -0400

    Navbar fixed hopefully?

--- src/App.js
+++ src/App.js
@@ -48,9 +48,9 @@ function App() {
         <Route path="/register" element={<Register setUser={setUser} />}/>
         <Route path="/login" element={<Login setUser={setUser} setProfile={setProfile}/>} />
         <Route path="/sign-out" element={<SignOut setUser={setUser} setProfile={setProfile}/>} />
-        <Route path="/profile/:id?" element={<Profile />} />
+        <Route path="/profile/:id?" element={<Profile setUser={setUser}/>} />
         {/* <Route path="/profile" element={<Profile />} /> */}
-        <Route path="/profile/edit" element={<ProfileEdit />} />
+        <Route path="/profile/edit" element={<ProfileEdit setUser={setUser}/>} />
         <Route path="/matched-page" element={<MatchedPage profile={profile}/>} />
         {profile && <Route path="/matching-page" element={<MatchingPage profile={profile} setProfile={setProfile}/>} />}
         <Route path="/about" element={<About user={user}/>}/>
