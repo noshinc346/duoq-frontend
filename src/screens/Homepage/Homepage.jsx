8d87e05a14a57e613e24e630ba337ed1e2419127
Jenn Bourke <jennfbourke@gmail.com>
2024-04-15 01:22:03 -0400

    css edits done for homepage, matching page, landing page

--- src/screens/Homepage/Homepage.jsx
+++ src/screens/Homepage/Homepage.jsx
@@ -4,6 +4,7 @@ import { getGames } from '../../services/games.js';
 import { getUserId } from '../../services/user.js';
 import { getUserGame } from '../../services/usergame.js';
 import AddUserGame from '../../components/Modals/AddUserGame.jsx';
+import HPControllers from '../../assets/hpcontrollers.jpeg'
 
 function Homepage() {
 
@@ -29,23 +30,17 @@ function Homepage() {
   return (
     <div className="homepage">
 	<div className="hp-container">
+	<img className="hp-img" src={HPControllers} />
 		<div className="hp-container-one">
-          		<p className="intro">
-          			 Welcome Back to DuoQ
-          		</p>
-        	</div>
-        	<div className="intro-statement-container">
-          		<p className="intro-statement">
-          			DuoQ is your go-to app for finding your gaming soulmate.
-           			<br/>
-           			You: {userGame[0]?.profile?.bio}	
-          		</p>
+          		<h1 className="intro">
+          			 Welcome Back to DuoQ, {userGame[0]?.profile?.name}
+          		</h1>
         	</div>
 		<div className="hp-container-three">
-				<h1 className="games-header">
+				<h1 className="games-header-hp">
 	  				Your Games
 	  			</h1>
-        		<div className = "img-gallery">
+        		<div className="hp-img-gallery">
 	  			{userGame.map((usergame, idx) => {
 					return (
 						<img key={idx} className="gallery-imgs" src={usergame.game.image}/>
